c2f8264cacc13a0b6d3c2f39069f65a38c98826a
subarnoturja <subarnoturja15@gmail.com>
2024-02-15 01:40:17 +0600

    button color changing effect added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -8,10 +8,32 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key; // event.key use krle ami ki press kortisi oita javascript capture kortese 
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, currentAlphabet);
+    // console.log(currentAlphabetElement.innerText);
+
+    // check matched or not 
+    if(playerPressed === expectedAlphabet){
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you lost a life');
+    }
+}
+
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame(){
     // step 1 --> generate a random alphabet
     const alphabet = getRandomAlphabet();
-    console.log('your random alphabet',alphabet);
 
     // set randomly generated alphabet to the screen to show
     const currentAlphabetElement = document.getElementById('current-alphabet');
