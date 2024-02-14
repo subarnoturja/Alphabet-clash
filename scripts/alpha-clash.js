be43d602e8e6a47d9a8329c4550d7fcadd4bd51e
subarnoturja <subarnoturja15@gmail.com>
2024-02-15 01:58:37 +0600

    added score and life

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -20,11 +20,33 @@ function handleKeyboardButtonPress(event){
 
     // check matched or not 
     if(playerPressed === expectedAlphabet){
+        // update score:
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+        // start a new round 
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else{
-        console.log('you lost a life');
+        // step 1 --> get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // step 2 --> reduce the life count 
+        const newLife = currentLife - 1;
+
+        // step 3 --> display the how many life left
+        currentLifeElement.innerText = newLife;
     }
 }
 
